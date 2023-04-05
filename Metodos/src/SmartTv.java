9a7c5107be70c259e084f1ecae74f5d56be1a159
leonardoalvs1 <alvesleonardo053@gmail.com>
2023-04-05 20:18:39 -0300

    criando projeto smart-tv

---
+++ Metodos/src/SmartTv.java
@@ -0,0 +1,13 @@
+public class SmartTv {
+    
+    boolean ligada = true;
+    int canal = 1;
+    int volume = 23;
+
+
+    public void ligar(){
+        
+    }
+
+    
+}
---
+++ Metodos/src/Usuario.java
@@ -0,0 +1,6 @@
+public class Usuario {
+    public static void main(String[] args){
+        
+    }
+    
+}
